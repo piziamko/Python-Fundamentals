cc22d4ad99d86cc3a08af5b306237c5da4f133cd
piziamko <stoyanovasibina@gmail.com>
2021-04-22 16:29:13 +0300

    Update 03. Pth_bit.py

--- 11. Bitwise Operations/03. Pth_bit.py
+++ 11. Bitwise Operations/03. Pth_bit.py
@@ -1,24 +1,6 @@
-num = int(input())
-list_1 = []
-list_2 = []
-for i in range(num):
-    num1 = int(input())
-    list_1.append(num1)
-key = input()
-if key == "even":
-    for l in list_1:
-        if l % 2 == 0:
-            list_2.append(l)
-elif key == "odd":
-    for l in list_1:
-        if l % 2 != 0:
-            list_2.append(l)
-elif key == "negative":
-    for l in list_1:
-        if l < 0:
-            list_2.append(l)
-else:
-    for l in list_1:
-        if l >= 0:
-            list_2.append(l)
-print(list_2)
+number = int(input())
+position = int(input())
+
+result = (number >> position) & 1
+
+print(result)
